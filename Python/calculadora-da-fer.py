8eb2fb63109438ee1e1131bb4642f10de52b59cc
Fernanda Soares <32881250+fersl@users.noreply.github.com>
2023-10-10 14:42:48 -0300

    adicionando cálculo de soma

--- Python/calculadora-da-fer.py
+++ Python/calculadora-da-fer.py
@@ -1,2 +1,11 @@
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
+
+op = int(input("Escolha uma operação: \n"
+               "1 - soma"
+               "2- subtração"
+               "3 - multiplicação"
+               "4 - divisão"))
+
+if op == 1:
+    print(n1+n2)
