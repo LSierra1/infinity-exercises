2f317230f883318988880b632b0c3c6d444cf3ee
Lucas Sierra <96590298+LSierra1@users.noreply.github.com>
2023-10-12 16:56:22 -0300

    Done

---
+++ Python/Aula 5/questao04.py
@@ -0,0 +1,7 @@
+listString = ["cAt", "hOUse", "sUnshine", "ElePhant", "AppLe", "CoLor", "MoUntain", "REmote", "ComPuter", "GEnius"]
+
+# upperListString = list(map(lambda listString: listString.upper(), listString))
+
+# print(upperListString)
+
+print(list(map(lambda listString: listString.upper(), listString)))
